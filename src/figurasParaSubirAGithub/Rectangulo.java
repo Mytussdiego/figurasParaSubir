41b233c61f02b39fd02504d6ae4f9eabd7c8ec6b
Diego <mytussdiego@gmail.com>
2021-10-04 17:12:18 -0300

    Se agrega constructor y atributos

--- src/figurasParaSubirAGithub/Rectangulo.java
+++ src/figurasParaSubirAGithub/Rectangulo.java
@@ -1,5 +1,11 @@
 package figurasParaSubirAGithub;
 
 public class Rectangulo {
+private double ancho;
+private double alto;
 
+public Rectangulo(double ancho, double alto) {
+	this.ancho = ancho;
+	this.alto = alto;
+}
 }
